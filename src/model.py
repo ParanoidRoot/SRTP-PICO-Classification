2491304543eda3d769d85eebdab59fc227a3ca8b
ParanoidRoot <578791680@qq.com>
2019-08-15 00:39:25 +0800

    :wrench: Update .exe.
    增加了编译环境.

--- src/model.py
+++ src/model.py
@@ -1,8 +1,14 @@
-from src.util import get_elements, ele_dir
+from src.util import get_elements, ele_dir, pre_model_path
+from gensim.models.keyedvectors import KeyedVectors
+import random
 
 
 if __name__ == '__main__':
-    elements = get_elements(ele_dir)
-    import random
-    index = random.randint(1, 1000)
-    print(elements[index])
+    pico_elements = get_elements(ele_dir)
+    element_number = len(pico_elements)
+    # pre_model = KeyedVectors.load_word2vec_format(
+    #     pre_model_path, binary=True
+    # )
+    index = random.randint(1, element_number)
+    element = pico_elements[index]
+    print(index, '\r\n', element)
