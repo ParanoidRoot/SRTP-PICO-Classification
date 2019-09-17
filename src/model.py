35151c9aa68ba63a9d766a7c0f25246cae96a819
ParanoidRoot <578791680@qq.com>
2019-09-17 21:08:30 +0800

    :memo: Test using.

--- src/model.py
+++ src/model.py
@@ -1,10 +1,10 @@
-from src.util import get_elements, ele_dir, pre_model_path
+from src.util import get_all_elements, ele_dir, pre_model_path
 from gensim.models.keyedvectors import KeyedVectors
 import random
 
 
 if __name__ == '__main__':
-    pico_elements = get_elements(ele_dir)
+    pico_elements = get_all_elements(ele_dir)
     element_number = len(pico_elements)
     # pre_model = KeyedVectors.load_word2vec_format(
     #     pre_model_path, binary=True
